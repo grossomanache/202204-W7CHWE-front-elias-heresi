5961fa66f1c1489a04c4b4254272d0f138b27085
Elías Heresi <elias.heresi@gmail.com>
2022-05-21 23:53:57 +0200

    Created login component

--- src/redux/thunks/thunks.js
+++ src/redux/thunks/thunks.js
@@ -1,6 +1,18 @@
 import axios from "axios";
+import jwtDecode from "jwt-decode";
+import { loginActionCreator } from "../features/usersSlice";
 
 export const registerUserThunk = (registerInformation) => async (dispatch) => {
   const route = `${process.env.REACT_APP_API_URL}/users/register`;
   await axios.post(route, registerInformation);
+};
+
+export const loginUserThunk = (loginInformation) => async (dispatch) => {
+  const route = `${process.env.REACT_APP_API_URL}/users/login`;
+  const {
+    data: { token },
+  } = await axios.post(route, loginInformation);
+  localStorage.setItem("token", token);
+  const userInfo = jwtDecode(token);
+  dispatch(loginActionCreator(userInfo));
 };
